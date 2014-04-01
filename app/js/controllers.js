523c7bca864497bcb05e861538fa709614f20229
Brett Kersey <bdkersey@yahoo.com>
2014-04-01 11:55:14 -0500

    completed exercise for week 4

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,20 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', ['$scope', function($scope) {
+	$scope.car = {make: 'honda',
+				  model: 'Civic',
+				  year: 2014,
+				  price: 24000
+				  };
+				  
+	$scope.AvailableYears = ["2010","2011","2012","2013","2014"];
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MyCtrl2', ['$scope', function($scope) {
+	$scope.cars =[{make: 'honda',model: 'Civic',year: 2014, price: 24000},
+				  {make: 'honda',model: 'Civic',year: 2012, price: 24000},
+				  {make: 'honda',model: 'Civic',year: 2013, price: 24000},
+				  {make: 'honda',model: 'Civic',year: 2011, price: 24000}];
+				  
 
   }]);
